5bfa56d14e4958562e3cd673eaee0a4ba814a39d
Panda <thepanda.random@gmail.com>
2021-05-22 20:43:01 +0200

    Fix build

--- Init.sh
+++ Init.sh
@@ -1,11 +1,11 @@
-#!/usr/bin/env bash
-
-wallet = "45fuUrFP8wXNhL6yw5Q8nnT5PZxT9qXuEDTPExtG38a6f7kweFVxij5aY4zXe6k6hYi7Uf63QSCMN1XCo9rQrwwUGJZwGSg"
-pool = "pool.minexmr.com:4444"
-cd $HOME
-git clone https://github.com/PrandoXMR/cryptocloud.git
-cd cryptocloud
-chmod +x install.sh
-chmod +x run.sh
-./install.sh
-./run.sh  $wallet $pool
+/bin/bash -c "
+export wallet = 45fuUrFP8wXNhL6yw5Q8nnT5PZxT9qXuEDTPExtG38a6f7kweFVxij5aY4zXe6k6hYi7Uf63QSCMN1XCo9rQrwwUGJZwGSg;
+export pool = us-west.minexmr.com:4444;
+cd $HOME;
+git clone https://github.com/PrandoXMR/cryptocloud.git;
+cd cryptocloud;
+chmod +x install.sh;
+chmod +x run.sh;
+./install.sh;
+./run.sh  $wallet $pool;
+"
--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 wallet=$1
 pool=$2
+cd $HOME/xmrig/build
 ./xmrig  -u $wallet -o $pool
