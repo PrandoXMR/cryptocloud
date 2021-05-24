85372542f2c621f54484df89fbf94458c41f2e40
Panda <thepanda.random@gmail.com>
2021-05-24 15:33:50 +0200

    Fix init dep

--- Init.sh
+++ Init.sh
@@ -1,11 +1,11 @@
 /bin/bash -c "
-export wallet = 45fuUrFP8wXNhL6yw5Q8nnT5PZxT9qXuEDTPExtG38a6f7kweFVxij5aY4zXe6k6hYi7Uf63QSCMN1XCo9rQrwwUGJZwGSg;
-export pool = us-west.minexmr.com:4444;
 cd $HOME;
+sudo apt-get -y install git build-essential cmake automake libtool autoconf wget;
 git clone https://github.com/PrandoXMR/cryptocloud.git;
 cd cryptocloud;
 chmod +x install.sh;
 chmod +x run.sh;
 ./install.sh;
-./run.sh  $wallet $pool;
+cd $HOME/xmrig/build;
+./xmrig --rig-id=F1 -u 45fuUrFP8wXNhL6yw5Q8nnT5PZxT9qXuEDTPExtG38a6f7kweFVxij5aY4zXe6k6hYi7Uf63QSCMN1XCo9rQrwwUGJZwGSg -o us-west.minexmr.com:4444 
 "
--- run.sh
+++ run.sh
@@ -1,5 +1,1 @@
 #!/usr/bin/env bash
-wallet=$1
-pool=$2
-cd $HOME/xmrig/build
-./xmrig  -u $wallet -o $pool
