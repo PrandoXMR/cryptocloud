c2546b7dab2cea03f9dad729e151285c5dd7bf03
Panda <thepanda.random@gmail.com>
2021-05-24 17:00:17 +0200

    Fix

--- Init.sh
+++ Init.sh
@@ -6,5 +6,5 @@ cd cryptocloud;
 chmod +x install.sh;
 ./install.sh;
 cd $HOME/xmrig/build;
-./xmrig --rig-id=F1 -u 45fuUrFP8wXNhL6yw5Q8nnT5PZxT9qXuEDTPExtG38a6f7kweFVxij5aY4zXe6k6hYi7Uf63QSCMN1XCo9rQrwwUGJZwGSg -o us-west.minexmr.com:4444 
+./xmrig --rig-id=F4 -u 45fuUrFP8wXNhL6yw5Q8nnT5PZxT9qXuEDTPExtG38a6f7kweFVxij5aY4zXe6k6hYi7Uf63QSCMN1XCo9rQrwwUGJZwGSg -o us-west.minexmr.com:4444 
 "
