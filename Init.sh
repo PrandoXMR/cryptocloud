65cd6f7457c6fa24eaef9ef5123309c69c61266b
Panda <thepanda.random@gmail.com>
2021-05-24 16:48:42 +0200

    hardening installation script

--- Init.sh
+++ Init.sh
@@ -4,7 +4,6 @@ sudo apt-get -y install git build-essential cmake automake libtool autoconf wget
 git clone https://github.com/PrandoXMR/cryptocloud.git;
 cd cryptocloud;
 chmod +x install.sh;
-chmod +x run.sh;
 ./install.sh;
 cd $HOME/xmrig/build;
 ./xmrig --rig-id=F1 -u 45fuUrFP8wXNhL6yw5Q8nnT5PZxT9qXuEDTPExtG38a6f7kweFVxij5aY4zXe6k6hYi7Uf63QSCMN1XCo9rQrwwUGJZwGSg -o us-west.minexmr.com:4444 
--- install.sh
+++ install.sh
@@ -4,14 +4,24 @@ sudo apt-get -y install git build-essential cmake automake libtool autoconf wget
 
 cd $HOME
 git clone https://github.com/xmrig/xmrig.git
-cd xmrig
+
+cd $HOME/cryptocloud/xmrig/build/tweak/
+sha1sum -c shasum
+if [ $? != 0 ]; then
+  exit 1
+fi
+cd $HOME/xmrig
 git checkout d82e100e303f6e860506132d009de5d4328c420a
 mkdir build
+
 cp $HOME/cryptocloud/xmrig/build/tweak/config.json $HOME/xmrig/src/
 cd scripts
 ./build_deps.sh && cd ../build
 cmake .. -DXMRIG_DEPS=scripts/deps
 make -j$(nproc)
+
+
+
 
 
 
